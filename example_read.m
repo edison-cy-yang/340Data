cac220b6c5d8ebbc09f27489cb2d406c960af983
Edison Yang <edison.cy.yang@gmail.com>
2015-11-18 02:28:27 -0800

    Load tables into the workspace

--- example_read.m
+++ example_read.m
@@ -1,4 +1,8 @@
 load businesslicences.mat
+load businesslicences2011.mat
+load businesslicences2012.mat
+load businesslicences2013.mat
+load businesslicences2014.mat
 
 %test code to see the number of issued businesses
 c = ones(60251,1);
